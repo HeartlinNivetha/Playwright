74341a6d57288157e86633c5f74de2038dd5d0ae
HeartlinNivetha <heartynive91@gmail.com>
2024-09-27 22:00:00 +0530

    Update draganddrop.test.ts

--- draganddrop.test.ts
+++ draganddrop.test.ts
@@ -5,9 +5,9 @@ import { chromium } from 'playwright/test';
     
     const browser = await chromium.launch({ headless: false });  // Launches the browser in non-headless mode (visible)
     const page = await browser.newPage();                        // Creates a new browser tab/page
-    await page.goto('https://practice.expandtesting.com/');    // Navigates to the given URL 
+    await page.goto('https://practice.expandtesting.com/');      // Navigates to the given URL 
     await page.goto('https://practice.expandtesting.com/drag-and-drop'); 
     await page.dragAndDrop('xpath=//*[@id="column-a"]', 'xpath=//*[@id="column-b"]');
-    //await page.waitForTimeout(2000); // Wait to observe the result
-    //await browser.close();                                   // Closes the browser after the title is logged
+    await page.waitForTimeout(4000); // Wait to observe the result
+    await browser.close();                                   // Closes the browser after the title is logged
 })()
